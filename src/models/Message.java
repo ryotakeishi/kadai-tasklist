9611e7dc6e97ef27e136d5b9852cad4c697e1064
riyou.takeishi <RIYOU.TAKEISHI@grp.tmnf.jp>
2023-05-15 11:22:05 +0900

    テーブル名の変更

--- src/models/Message.java
+++ src/models/Message.java
@@ -21,7 +21,7 @@ import javax.persistence.Table;
             query = "SELECT COUNT(m) FROM Message AS m"
             )
 })
-@Table(name = "tasklist")
+@Table(name = "tasks")
 public class Message {
     @Id
     @Column(name = "id")
